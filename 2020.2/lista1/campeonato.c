d57e9c92b48a08f5a255f41c5c82e60d6c63a6f6
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2022-07-26 17:09:25 -0300

    Update campeonato.c

--- 2020.2/lista1/campeonato.c
+++ 2020.2/lista1/campeonato.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+// BIBLIOTECAS ADICIONADAS
 
-int main(){
-    
-    return 0;
+int main() {
+	// VARIAVEIS CRIADAS
+	int cv, ce, cs, fv, fe, fs, cp, fp;
+	
+	// DADOS ADICIONADOS
+	fflush(stdin);
+	scanf(" %i %i %i %i %i %i", &cv, &ce, &cs, &fv, &fe, &fs);
+	fflush(stdin);
+	
+	// CALCULOS DOS PONTOS
+	cp = cv*3 + ce;
+	fp = fv*3 + fe;
+	
+	// RESULTADOS APRESENTADOS
+	if (cp > fp){
+		printf("C");
+	}else if (cp < fp){
+		printf("F");
+	}else if (cp == fp){
+		if (cs > fs){
+			printf("C");
+		}else if (cs < fs){
+			printf("F");
+		}else if (cs == fs){
+			printf("=");
+		}
+	}
+	
+	return 0;
 }
