15d5c614e91f25f48c4f2c4a64a331486d232bb6
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2022-07-26 19:58:52 -0300

    Create a_saga_do_protagonista.c

---
+++ 2020.2/lista1/a_saga_do_protagonista.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+// BIBLIOTECAS ADICIONADAS
+
+int main() {
+	int n1, n2, dif, resto, div2, div3, total;
+	
+	scanf(" %i %i", &n1, &n2);
+	
+	dif = fabs(n1-n2);
+	div3 = dif/3;
+	resto = dif - div3*3;
+	
+	if(resto==0){
+		printf("%i", div3);
+	}else if(resto==1){
+		if((n1==n2+1)||(n2==n1+1)){
+			printf("2");
+		}else{
+			div3 = div3 - 1;
+			div2 = 2;
+			total = div3 + div2;
+			printf("%i", total);
+		}
+	}else if(resto==2){
+		div2 = 1;
+		total = div3 + div2;
+		printf("%i", total);
+	}
+	
+	return 0;
+}
