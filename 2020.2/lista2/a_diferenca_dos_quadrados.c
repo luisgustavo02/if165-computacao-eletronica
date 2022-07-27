1682ce14414ffa663b883898b91a0bfa2a3232d1
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2022-07-27 10:06:21 -0300

    Update and rename a-diferenca-dos-quadrados.c to a_diferenca_dos_quadrados.c

---
+++ 2020.2/lista2/a_diferenca_dos_quadrados.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+// BIBLIOTECAS ADICIONADAS
+
+int main() {
+    // VARIAVEIS CRIADAS
+	int num, n1, n2, pot1, pot2;
+	
+    // CICLO COM CONDICIONAL
+	while(1){
+        // INSERIR VARIAVEL E CONDICIONAL
+		scanf(" %i", &num);
+		if(num==0){
+			break;
+		}
+        
+        // CALCULO DOS QUADRADOS
+		n1 = num/2;
+		n2 = (num/2)+1;
+		pot1 = pow(n1,2);
+		pot2 = pow(n2,2);
+        
+        // RESULTADOS
+		printf("%i - %i\n", pot2, pot1);
+	}
+  
+	return 0;
+}
