4225e5b77b9cd2f651491c140953c89baa94b028
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2022-07-28 23:54:48 -0300

    Update frequencia_de_um_numero.c

--- 2020.2/lista2/frequencia_de_um_numero.c
+++ 2020.2/lista2/frequencia_de_um_numero.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+// https://www.thehuxley.com/problem/2843
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
 //BIBLIOTECAS ADICIONADAS
 
 int main() {
