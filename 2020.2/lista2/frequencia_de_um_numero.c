38d31ac790ddad8b0110c7dca483e60b3238524c
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2022-07-28 23:37:03 -0300

    Create frequencia_de_um_numero.c

---
+++ 2020.2/lista2/frequencia_de_um_numero.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+//BIBLIOTECAS ADICIONADAS
+
+int main() {
+    // VARIAVEIS CRIADAS
+	int vez, num, esc, apar;
+	
+    // PERGUNTAS NA TELA E DADOS SENDO RECEBIDOS
+	printf("Digite a quantidade de numeros da sequencia:\n");
+	scanf(" %i", &vez);
+	printf("Digite o numero procurado:\n");
+	scanf(" %i", &num);
+	
+    // CHECAGEM NA APARICAO DO NUMERO
+	apar = 0;
+	while(vez!=0){
+		scanf(" %i", &esc);
+		vez = vez - 1;
+		if(esc==num){
+			apar = apar + 1;
+		}
+	}
+	
+    // IMPRIMIR RESULTADO NA TELA
+	printf("O numero %i apareceu na sequencia %i vezes.", num, apar);
+	
+	return 0;
+}
