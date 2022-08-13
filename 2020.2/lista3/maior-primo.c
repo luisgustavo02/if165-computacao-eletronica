34bc684fc519d4c94d669e231130749ecb624384
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-08-13 19:47:05 -0300

    Update maior-primo.c

--- 2020.2/lista3/maior-primo.c
+++ 2020.2/lista3/maior-primo.c
@@ -1,2 +1,51 @@
 // https://www.thehuxley.com/problem/470
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+// BIBLIOTECAS ADICIONADAS
+
+int main(){
+  // VARIAVEL NUM CRIADA
+	int num;
+	
+  // PRIMEIRA ENTRADA
+	scanf(" %d", &num);
+	
+  // VARIAVEIS DO CICLO CRIADAS
+	int i, maiorPrimo = 0, aux;
+  
+  // CICLO WHILE COM CONDIÇÃO DE num != 404
+	while(num != 404){
+    // DEFININDO VARIAVEL AUXILIAR
+		aux = 0;
+    
+    // CICLO PARA CHECAGEM DE DIVISORES DE num
+		for(i=2; i<num; i++){
+			if(num % i == 0){
+				aux++;
+			}
+		}
+    
+    // CONDIÇÃO PARA CASO num SEJA O MAIOR PRIMO
+		if(aux == 0 && num > 1 && num > maiorPrimo){
+			maiorPrimo = num;
+		}
+		
+    // ENTRADA DE UM NOVO NÚMERO
+		scanf(" %d", &num);
+	}
+	
+  // CASO TENHA PRIMOS
+	if(maiorPrimo != 0){
+		printf("%d", maiorPrimo);
+	}
+  
+  // CASO N
+  else{
+		printf("SEM PRIMOS");
+	}
+	
+	return 0;
+}
