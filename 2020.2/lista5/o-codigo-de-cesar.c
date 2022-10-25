38552d24f139186a93d8ac5979309127ba56602e
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-10-25 10:01:40 -0300

    Update o-codigo-de-cesar.c

--- 2020.2/lista5/o-codigo-de-cesar.c
+++ 2020.2/lista5/o-codigo-de-cesar.c
@@ -1,1 +1,36 @@
 // https://www.thehuxley.com/problem/3629
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+// BIBLIOTECAS ADICIONADAS
+
+int main(){
+    // VARIAVEL K DE ACRESCIMO
+    int indice;
+    
+    // FRASE A SER CRIPTOGRAFADA
+    char codigo[30];
+    
+    // ENTRADA DA FRASE
+    scanf(" %[^\n]s", codigo);
+    
+    // ENTRADA DE K
+    scanf(" %d", &indice);
+    
+    // CICLO PERCORRENDO A FRASE
+    for(int i=0; codigo[i]!='\0';i++){
+        // TRANSFORMANDO A LETRA EM MINUSCULA
+        codigo[i] = tolower(codigo[i]);
+        
+        // ADICIONANDO O INDICE
+        codigo[i] += indice;
+        
+        // IMPRIMINDO A LETRA
+        printf("%c", codigo[i]);
+    }
+    
+    return 0;
+}
